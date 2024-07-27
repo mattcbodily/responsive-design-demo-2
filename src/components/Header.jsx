806f6728248912246f7aa3cdd72b48e5caeb2763
mattcbodily <mattcbodily@gmail.com>
2024-07-27 10:57:27 -0600

    changes header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import { MobileMenu } from './MobileMenu'
 
 export const Header = () => {
   return (
-    <header className='h-16 px-6 relative flex items-center justify-between bg-indigo-700 text-slate-100'>
-      <p className='text-lg'>Handsome Squidward</p>
+    <header className='h-16 px-6 relative flex items-center justify-between bg-gradient-to-br from-indigo-700 to-teal-700 text-slate-100 drop-shadow-lg'>
+      <p className='text-lg tracking-wider font-bold'>Handsome Squidward</p>
       {/* <nav className='space-x-6'>
         <span className='cursor-pointer hover:text-indigo-200'>Home</span>
         <span className='cursor-pointer hover:text-indigo-200'>About</span>
