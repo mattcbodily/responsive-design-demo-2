a020b5821862c7d9bb1d43b8047c675896c83c19
mattcbodily <mattcbodily@gmail.com>
2024-07-27 12:55:35 -0600

    finished tailwind demo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import { MobileMenu } from './MobileMenu'
 
 export const Header = () => {
   return (
-    <header className='h-16 px-6 relative flex items-center justify-between bg-gradient-to-br from-indigo-700 to-teal-700 text-slate-100 drop-shadow-lg'>
+    <header className='h-16 px-6 relative lg:fixed lg:top-0 lg:left-0 lg:w-full flex items-center justify-between bg-gradient-to-br from-indigo-700 to-teal-700 text-slate-100 drop-shadow-lg'>
       <p className='text tracking-wider font-bold'>Handsome Squidward</p>
-      {/* <nav className='space-x-6'>
+      <nav className='space-x-6 hidden md:inline-block'>
         <span className='cursor-pointer hover:text-indigo-200'>Home</span>
         <span className='cursor-pointer hover:text-indigo-200'>About</span>
         <span className='cursor-pointer hover:text-indigo-200'>Contact</span>
-      </nav> */}
+      </nav>
       <MobileMenu />
     </header>
   )
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,15 @@ import heroImage from '../assets/handsome-squidward.jpg'
 
 export const Hero = () => {
   return (
-    <div className='p-12 flex flex-col items-center gap-y-16'>
+    <div className='p-12 flex flex-col lg:flex-row lg:justify-between items-center gap-y-16 lg:h-screen'>
       <img  
         alt=""
-        className='h-[250px] w-[250px] rounded-full object-cover drop-shadow-lg'
+        className='h-[250px] md:h-[350px] w-[250px] md:w-[350px] rounded-full object-cover drop-shadow-lg'
         src={heroImage}
       />
       <div className='text-center'>
-        <h1 className='mb-4 text-3xl font-bold'>Hi, I'm Handsome Squidward</h1>
-        <p className='text-sm text-slate-700'>Welcome to my responsive portfolio</p>
+        <h1 className='mb-4 text-3xl md:text-5xl font-bold'>Hi, I'm Handsome Squidward</h1>
+        <p className='text-sm md:text-lg text-slate-700'>Welcome to my responsive portfolio</p>
       </div>
     </div>
   )
